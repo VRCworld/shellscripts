5529926112ee5f0fe59db4cc18f073e6b63cf114
VRCworld <100361600+VRCworld@users.noreply.github.com>
2022-03-15 21:49:33 +0530

    Add files via upload

---
+++ shellscripts/16.function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Purpose: Function
+
+function Welcome () {
+echo "SrimanIT courses $1"
+}
+
+#calling Fuction
+
+Welcome datascienc
+Welcome powerbi
+Welcome Linux
+Welcome AWS
+Welcome DevOps
